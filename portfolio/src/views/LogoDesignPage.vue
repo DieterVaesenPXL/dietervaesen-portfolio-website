040a79eec1031a2f9d40b6fde6e6ed962228c612
Dieter Vaesen <12102436@student.pxl.be>
2026-05-18 12:58:11 +0200

    fix: remove empty Photography.vue and LogoDesignPage.vue

--- portfolio/src/views/LogoDesignPage.vue
+++ portfolio/src/views/LogoDesignPage.vue
@@ -1,11 +1,5 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <main>
+    <h1>Logo</h1>
+  </main>
 </template>
-
-<style scoped>
-
-</style>
---
+++ portfolio/src/views/PhotographyPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Photography</h1>
+  </main>
+</template>
